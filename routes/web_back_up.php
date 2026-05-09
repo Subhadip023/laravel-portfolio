2461e410706e2a2fca60f4fe2bedff1d621765a8
Subhadip023 <subhadip240420@gmail.com>
2026-05-09 20:26:44 +0530

    feat: Add new Blade components and authentication routes
    - Created new Blade components for buttons, dropdowns, input fields, and modals.
    - Implemented a dashboard view and guest layout.
    - Established navigation layout with responsive design.
    - Added profile management views and forms for updating user information, password, and account deletion.
    - Set up authentication routes for registration, login, password reset, and email verification.
    - Developed feature tests for authentication, email verification, password management, and profile updates.

--- routes/web_back_up.php
+++ routes/web_back_up.php
@@ -1,23 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 
 Route::get('/', function () {
     return view('index');
@@ -51,6 +34,3 @@ Route::get('/admin/blogs/create', function () {
     return view('admin.blogs.create');
 })->name('admin.blogs.create');
 
-
-
-require __DIR__.'/auth.php';
