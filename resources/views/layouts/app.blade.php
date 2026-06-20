0115e7be25acf9632722be70aff28f19145577de
debashismidya <debashis.matainja@gmail.com>
2026-06-20 08:52:18 +0530

    add instragram and linkedin links to footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,14 +97,14 @@
                         </path>
                     </svg>
                 </a>
-                <a class="ml-3 text-gray-500" target="_blank" href="#">
+                <a class="ml-3 text-gray-500" target="_blank" href="https://www.instagram.com/iamsubhadip2000/">
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         class="w-5 h-5" viewBox="0 0 24 24">
                         <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                         <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                     </svg>
                 </a>
-                <a class="ml-3 text-gray-500" target="_blank" href="#">
+                <a class="ml-3 text-gray-500" target="_blank" href="https://www.linkedin.com/in/subhadip-chakraborty-988bb4205/">
                     <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"
                         class="w-5 h-5" viewBox="0 0 24 24">
                         <path stroke="none"
